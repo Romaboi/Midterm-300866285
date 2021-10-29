e626049f2ac5fa81d73830c60e786d5362ecc186
Roman Gagarin <gagarin.roma@gmail.com>
2021-10-29 12:21:53 -0400

    Configured database with Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,14 @@
+/* 
+File name: book.js
+Student Name: Roman Gagarin
+Student #: 300866285
+Date: October 29th, 2021
+*/
+
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  //"URI": "mongodb://localhost/books229"
+
+  //MongoDB Atlas deployment
+  "URI": "mongodb+srv://roman:W3mwxM3fZKjql2jm@midterm.xrziy.mongodb.net/midterm?retryWrites=true&w=majority"
 };
